b706560e4c2472faa70d003d38d0c45546afa6bd
Menthy <wumengxuan0728@gmail.com>
2022-11-07 01:20:44 -0800

    add footer

--- pages/index.js
+++ pages/index.js
@@ -24,10 +24,10 @@ export default function Home() {
       </main>
 
       <footer className="flex h-24 w-full items-center justify-center border-t">
-        {/* <a><FaRegEnvelope className="text-6xl" /></a>
+        <a><FaRegEnvelope className="text-6xl" /></a>
         <a><FaInstagram className="text-6xl" /></a>
         <a><FaLinkedin className="text-6xl" /></a>
-        <a><FaGithub className="text-6xl" /></a> */}
+        <a><FaGithub className="text-6xl" /></a>
       </footer>
     </div>
   )
