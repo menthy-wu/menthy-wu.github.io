f3f2dbdf34678ca25e11e3dcfe1c3aa79d77b6b7
Menthy <wumengxuan0728@gmail.com>
2022-11-10 10:10:11 -0800

    import scrollmagic

--- components/landing.jsx
+++ components/landing.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { Controller, Scene } from "react-scrollmagic";
 
 
 const Landing = (pictureList) => {
     return (
         <div className="w-full">
             <div className="bg-black">
-                <img src="\backGroundVideo1\backgroundVideo-20.png"
-                    alt="bg pic"
-                    layout="fill"
-                    className="bg-cover"
-                /></div>
+                <Controller><Scene duration={9000} pin>{(progress) => {
+                    return (<img src="\backGroundVideo1\backgroundVideo-20.png"
+                        alt="bg pic"
+                        layout="fill"
+                        className="bg-cover"
+                    />)
+                }}</Scene></Controller>
+            </div>
             <div className="w-full inset-x-0 inset-y-1/3 absolute">
                 <p className=" font-montserrat text-6xl font-bold text-white">
                     This is{' '}
