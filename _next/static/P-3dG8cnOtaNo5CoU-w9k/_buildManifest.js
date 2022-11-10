39c7da96f233d51bb572f7afc1462eba993df0c1
menthy-wu <73259022+menthy-wu@users.noreply.github.com>
2022-11-10 18:18:48 +0000

    Deploying to gh-pages from @ menthy-wu/menthy-wu.github.io@98e025aa2eb78046dfa4cb8fab1378115ad2a5de 🚀

--- _next/static/P-3dG8cnOtaNo5CoU-w9k/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/1bfc9850-3ce79102dee72ced.js","static/chunks/pages/index-ded8e1de5071aeda.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
