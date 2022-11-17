53fe123dd1850bdac37305c874ac7670bac5ca42
menthy-wu <wumengxuan0728@gmail.com>
2022-11-17 00:32:29 -0800

    footer links

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,14 +1,27 @@
 import React from "react";
-import {FaRegEnvelope, FaInstagram, FaLinkedin, FaGithub}from "react-icons/fa";
+import {
+  FaRegEnvelope,
+  FaInstagram,
+  FaLinkedin,
+  FaGithub,
+} from "react-icons/fa";
 
 const Footer = () => {
-    return (
+  return (
     <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a><FaRegEnvelope className="text-6xl" /></a>
-        <a><FaInstagram className="text-6xl" /></a>
-        <a><FaLinkedin className="text-6xl" /></a>
-        <a><FaGithub className="text-6xl" /></a>
+      <a href="">
+        <FaRegEnvelope className="text-6xl" />
+      </a>
+      <a href="https://www.instagram.com/wmx_owo/">
+        <FaInstagram className="text-6xl" />
+      </a>
+      <a href="https://www.linkedin.com/in/mengxuan-wu/">
+        <FaLinkedin className="text-6xl" />
+      </a>
+      <a href="https://github.com/menthy-wu">
+        <FaGithub className="text-6xl" />
+      </a>
     </footer>
-    );
-}
+  );
+};
 export default Footer;
