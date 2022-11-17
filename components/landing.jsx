28ddc2eb6cb6a20bad5b09f3ea1417176d9d15b0
menthy-wu <wumengxuan0728@gmail.com>
2022-11-16 23:57:32 -0800

    add text stroke

--- components/landing.jsx
+++ components/landing.jsx
@@ -6,8 +6,8 @@ const Landing = ({ pictureList, picNum }) => {
     return (
         <div className="w-full bg-gray">
             <Nav />
+            <div className="font-anton text-15xl black-stroke">- THIS IS MENTHY WU -</div>
             <div className="font-anton text-15xl text-black">- THIS IS MENTHY WU -</div>
-            <div className="font-anton text-15xl ">- THIS IS MENTHY WU -</div>
         </div>
     );
 };
