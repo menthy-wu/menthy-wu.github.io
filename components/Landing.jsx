2345d9d29a5f2385176dfa65fd0cc62e21488698
menthy-wu <wumengxuan0728@gmail.com>
2022-11-17 21:10:12 -0800

    remove absolute

--- components/Landing.jsx
+++ components/Landing.jsx
@@ -6,35 +6,50 @@ const Landing = () => {
   return (
     <div className="w-full bg-gray flex flex-col items-center h-[700px]">
       <Nav />
-      <div className="relative w-full">
-        <div className="top-0 left-[100px] text-left absolute font-anton text-10xl black-stroke whitespace-nowrap text-clip overflow-hidden">
+      <div className="mt-6 w-full h-[105px] flex items-center overflow-hidden">
+        <div className="top-0 translate-x-[150px] text-left font-anton text-10xl black-stroke whitespace-nowrap text-clip">
           - THIS IS MENTHY WU -
         </div>
-        <div className="text-left top-[120px] left-[-350px] absolute font-abril text-xs text-black whitespace-nowrap text-clip overflow-hidden">
+      </div>
+      <div className="m-0 w-full h-[10px] flex items-center overflow-hidden">
+        <div className="text-left translate-x-[-60px] font-abril text-xs text-black whitespace-nowrap text-clip">
           This is jsut some random text. I just feel like it looks better if
           there is something. You can read it if you want, but it is just non
           sense. Also, if you read it in insector, you are a loser.
         </div>
-        <div className="top-[110px] left-[700px] absolute font-anton text-10xl text-black whitespace-nowrap text-clip overflow-hidden">
+      </div>
+      <div className="w-full h-[105px] flex items-center overflow-hidden">
+        <div className="font-anton translate-x-[700px] text-10xl text-black whitespace-nowrap text-clip overflow-hidden">
           - MENTHY WU -
         </div>
-        <div className="top-[220px] left-[-120px] absolute font-anton text-10xl text-black whitespace-nowrap text-clip overflow-hidden">
+      </div>
+
+      <div className="m-0 w-full h-[105px] flex items-center overflow-hidden">
+        <div className="font-anton translate-x-[-70px] text-10xl text-black whitespace-nowrap text-clip overflow-hidden">
           Computer Science at UCR
         </div>
-        <div className="top-[330px] left-[360px] absolute font-anton text-10xl black-stroke whitespace-nowrap text-clip overflow-hidden w-full">
+      </div>
+      <div className="m-0 w-full h-[105px] flex items-center overflow-hidden">
+        <div className="font-anton translate-x-[300px] text-10xl black-stroke whitespace-nowrap text-clip overflow-hidden w-full">
           Computer Science at UCR
         </div>
-        <div className="text-left top-[450px] left-[400px] absolute font-abril text-sm text-black whitespace-nowrap text-clip overflow-hidden w-full">
+      </div>
+      <div className="m-0 w-full h-[10px] flex items-center overflow-hidden">
+        <div className="text-left translate-x-[700px] font-abril text-xs text-black whitespace-nowrap text-clip">
           This is jsut some random text. I just feel like it looks better if
           there is something. You can read it if you want, but it is just non
           sense. Also, if you read it in insector, you are a loser.
         </div>
-        <div className="top-[490px] left-[900px] relative font-abril text-sm text-black w-1/6 text-right">
+      </div>
+      <div className="m-0 mt-3 w-full flex justify-end">
+        <div className="mx-20 w-[300px] h-[1px] bg-black absolute"></div>
+        <div className="relative">
+          <div className="w-[1px] h-[150px] bg-black absolute left-[310px] top-[-10px]"></div>
+        </div>
+        <div className="mx-24 font-abril text-sm text-black w-1/6 text-right">
           This is jsut some random text. I just feel like it looks better if
           there is something. You can read it if you want, but it is just non
           sense. Also, if you read it in insector, you are a loser.
-          <div className= "w-[300px] h-[1px] bg-black absolute top-0 left-[-30px]"></div>
-          <div className= "w-[1px] h-[150px] bg-black absolute top-[-20px] left-[220px]"></div>
         </div>
       </div>
     </div>
--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ import About from "../components/About"
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 w-full">
       <Head>
         <title>Menthy Wu</title>
         <meta name="description" content="Menthy Wu's personal website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex w-full flex-1 flex-col items-center justify-center text-center">
+      <main className="flex w-full flex-col items-center justify-center text-center">
         <Landing picNum={44}/>
         <About/>
       </main>
