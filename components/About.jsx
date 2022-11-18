0bd5193288effdb3d8010981c774030eb93e9610
Menthy <wumengxuan0728@gmail.com>
2022-11-18 00:31:07 -0800

    add about title

--- components/About.jsx
+++ components/About.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div className="bg-black w-full h-[700px]">
-      <div className="relative w-full">
-        {/* <div className="rotate-270 text-white font-abril text-10xl whitespace-nowrap text-clip overflow-hidden">About •About•About •</div> */}
+    <div className="bg-black w-full h-[700px] flex flex-row">
+      <div className="ml-10 w-[105px] h-full overflow-hidden justify-center items-center flex">
+        <div className="rotate-270 text-white font-abril text-10xl whitespace-nowrap text-clip ">About •About•About •</div>
+      </div>
+      <div className=" mx-[-20px] w-[105px] h-full overflow-hidden justify-center items-center flex">
+        <div className="rotate-90 white-stroke font-abril text-10xl whitespace-nowrap text-clip ">About •About•About •</div>
       </div>
     </div>
   );
