8abf63d1bedfa3b58abc6e1a6260fd64cacd71bb
menthy-wu <wumengxuan0728@gmail.com>
2022-11-26 00:23:25 -0800

    small changes

--- components/MenuButton.jsx
+++ components/MenuButton.jsx
@@ -89,6 +89,9 @@ const MenuButton = ({ setCursorVariant }) => {
         <motion.div
           onMouseEnter={() => setCursorVariant("onMovingButton")}
           onMouseLeave={() => setCursorVariant("default")}
+          onClick={() => {
+            setCursorVariant("default");
+          }}
           className="bg-nav rounded-full h-12 w-12 items-center justify-center flex font-abril p-2 text-xl"
           variants={variants}
           animate="default"
