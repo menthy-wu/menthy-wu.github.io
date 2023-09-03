3ddc6197303c2cb69ca4740706ac50e4439daac8
menthy-wu <wumengxuan0728@gmail.com>
2023-09-03 01:44:14 -0700

    art width

--- src/components/home/Arts.jsx
+++ src/components/home/Arts.jsx
@@ -11,7 +11,7 @@ const Arts = () => {
   return (
     <section
       id="arts"
-      className="justify-center flex flex-col w-10/12 md;w-6/12 my-10"
+      className="justify-center flex flex-col w-10/12 md:w-6/12 my-10"
     >
       <Title text="ARTS">
         <div className="h-6 w-24 absolute bg-custom-theme -left-4 -bottom-1 z-10" />
